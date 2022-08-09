724feda95729514a6671b40c904d02e829495526
Hai Nguyen H.P <hainhp@dipro.vn>
2022-08-09 21:21:02 +0700

    Move smart album on top Fix smart album not display when release app

--- ios/RNCCameraRollManager.m
+++ ios/RNCCameraRollManager.m
@@ -220,14 +220,14 @@ RCT_EXPORT_METHOD(getAlbums:(NSDictionary *)params
     };
 
   PHFetchOptions* options = [[PHFetchOptions alloc] init];
-  if ([albumType isEqualToString:@"Album"] || [albumType isEqualToString:@"All"]) {
-    fetchedAlbumType = @"Album";
-    PHFetchResult<PHAssetCollection *> *const assets = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAny options:options];
-    [assets enumerateObjectsUsingBlock:convertAsset];
-  }
   if ([albumType isEqualToString:@"SmartAlbum"] || [albumType isEqualToString:@"All"]) {
     fetchedAlbumType = @"SmartAlbum";
     PHFetchResult<PHAssetCollection *> *const assets = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeAny options:options];
+    [assets enumerateObjectsUsingBlock:convertAsset];
+  }
+  if ([albumType isEqualToString:@"Album"] || [albumType isEqualToString:@"All"]) {
+    fetchedAlbumType = @"Album";
+    PHFetchResult<PHAssetCollection *> *const assets = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAny options:options];
     [assets enumerateObjectsUsingBlock:convertAsset];
   }
 
@@ -409,8 +409,8 @@ RCT_EXPORT_METHOD(getPhotos:(NSDictionary *)params
             PHFetchResult<PHAsset *> *const assetsFetchResult = [PHAsset fetchAssetsInAssetCollection:assetCollection options:assetFetchOptions];
             currentCollectionName = [assetCollection localizedTitle];
             [assetsFetchResult enumerateObjectsUsingBlock:collectAsset];
+            *stopCollections = stopCollections_;
           }
-          *stopCollections = stopCollections_;
         }];
       } else {
         PHAssetCollectionSubtype const collectionSubtype = [RCTConvert PHAssetCollectionSubtype:groupTypes];
